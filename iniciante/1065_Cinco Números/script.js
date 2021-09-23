e10236c1a5ed5421da15c815c676cd54e8087756
Washington Albuquerque Moura <walbuque@gmail.com>
2021-09-23 16:46:42 -0300

    Ex_1065

---
+++ iniciante/1065_Cinco Números/script.js
@@ -0,0 +1,21 @@
+/*
+1060_Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.
+
+Entrada: Seis valores, negativos e/ou positivos.
+
+Saída: Imprima uma mensagem dizendo quantos valores positivos foram lidos.
+*/
+
+var input = require('fs').readFileSync('stdin', 'utf8');
+var lines = input.split('\n');
+
+var positive = 0;
+
+for (i=1; i<=5; i++){
+    var value = parseFloat(lines.shift());
+    if (value % 2 == 0){
+        positive++
+    }
+
+}
+console.log(`${positive} valores positivos` );
